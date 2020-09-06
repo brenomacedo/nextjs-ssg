2078aa1c29bdd58b613a1648ae6fb3421dcadbf3
brenosekiro <b88660501@gmail.com>
2020-09-06 08:37:03 -0300

    pagination

---
+++ src/pages/[currentPage].tsx
@@ -0,0 +1,22 @@
+import Index, { getStaticProps } from './index'
+import { GetStaticPaths } from 'next'
+import { openDB } from '../openDB'
+
+export default Index
+export {getStaticProps}
+
+export const getStaticPaths: GetStaticPaths<{ currentPage: string }> = async () => {
+
+    const db = await openDB()
+    const { total } = await db.get('select count(*) as total from microphone')
+    const numberOfPages = Math.ceil(total / 5)
+
+    const paths = Array(numberOfPages - 1).fill('').map((_, index) => {
+        return { params: { currentPage: (index + 1).toString() } }
+    })
+
+    return {
+        fallback: false,
+        paths
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,15 @@ import { Microphone } from "../../model/microphone"
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
 import { openDB } from "../openDB"
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 interface IndexProps {
     microphones: Microphone[]
@@ -9,10 +18,36 @@ interface IndexProps {
 
 export default function Index({ microphones }: IndexProps) {
     return (<div>
-        {microphones.map(microphone => (
-            <div>
-                <Link href="/[id]" as={`/${microphone.id}`}>
-                    <a>{microphone.brand} - {microphone.model}</a>
+        {microphones.map((microphone, index) => (
+            <div key={index}>
+                <Link href="/microphone/[id]" as={`/microphone/${microphone.id}`}>
+                    <a>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                            image={microphone.imageUrl}
+                            title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {`${microphone.brand} ${microphone.model}`}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                            <Button size="small" color="primary">
+                            Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                    </a>
                 </Link>
             </div>
         ))}
@@ -20,7 +55,13 @@ export default function Index({ microphones }: IndexProps) {
 }
 
 export const getStaticProps: GetStaticProps = async ctx => {
-    const db = openDB()
-    const microphones = await (await db).all('select * from microphone')
+    const currentPage = ctx.params?.currentPage || 0
+    const currentPageNumber = +(currentPage || 0)
+
+    const min = currentPageNumber * 5
+    const max = (currentPageNumber + 1) * 5
+    console.log(ctx.params)
+    const db = await openDB()
+    const microphones = await db.all('select * from microphone where id > ? and id <= ?', min, max)
     return { props: { microphones } }
 }
